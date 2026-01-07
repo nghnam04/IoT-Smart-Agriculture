3d15c51bb036bcfdc72a59f432d2c384153c3b2d
Nguyen Hoang Nam <hoangnam2004hp@gmail.com>
2026-01-07 16:27:38 +0700

    fix: resolve bug of statcard

--- src/components/dashboard/StatCard.jsx
+++ src/components/dashboard/StatCard.jsx
@@ -1,4 +1,4 @@
-import { AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 const StatCard = ({ title, value, unit, icon: Icon, colorClass }) => {
   return (
